55ee1fa329f78e25398e7acd2fafb21791c7569f
BrunaDevSis <brunasist@gmail.com>
2023-05-09 23:08:23 -0300

    finall

--- JS/script.js
+++ JS/script.js
@@ -1,25 +1,59 @@
-// --------------
 $(document).ready(function(){
-    if(localStorage.getItem("pedido") == true){
-        $("#off").hide(); 
-        $("#n-pedido").show(); 
-    } else {
-        $("#off").show(); 
-        $("#n-pedido").hide(); 
-    }
+    // localStorage.clear();   
 })
 
 $("#adicionar-produto").click(function(){
-    var nome_produto = $("#add-produto").html();
-    localStorage.setItem("pedido", nome_produto);
-    var teste = localStorage.getItem("pedido");
-    $("#off").hide(); 
-    $("#n-pedido").show(); 
-    $("#n-pedido").html(teste); 
-    $('#modal-pedido').modal('hide');
+    const andamento = document.getElementById('corpoTabela');
 
+    let arraypedidos = [];
+
+    if (localStorage.getItem('pedidos') != null) {
+        arraypedidos = JSON.parse(localStorage.getItem('pedidos'));
+    } else {
+        arraypedidos;
+    }
+
+    var nome_produto = $("#add-produto").html();
+    var val_unit_produto = $("#add-preco-input").val();
+    var val_total_produto = $("#add-total-input").val();
+    var qtde_produto = $("#quant").val();
+    var imagem_produto = $("#add-img").val();
+
+   
+    var info = {
+        nome_produto ,
+        val_unit_produto,
+        val_total_produto,
+        qtde_produto,
+        imagem_produto
+    };
+
+    arraypedidos.push(info);
+    var infoJson = JSON.stringify(arraypedidos);
+    localStorage.setItem("pedidos", infoJson);
+
+    const arr = JSON.parse(localStorage.getItem('pedidos'));
+    console.log(arr);
+
+    if (arr != null) {
+        let tr = '';
+        arr.map(conteudo => {
+        tr += `
+        <ul class="list-group">
+            <li class="list-group-item"><img src="${conteudo.imagem_produto}"  width="40" height="40"></img</li>
+            <li class="list-group-item">${conteudo.nome_produto }</li>
+            <li class="list-group-item">${conteudo.val_unit_produto}</li>
+            <li class="list-group-item">${conteudo.val_total_produto}</li>
+            <li class="list-group-item">${conteudo.qtde_produto}</li>
+        </ul>`
+        })
+  
+        $("#off").hide(); 
+        $("#on").show();  
+        $('#modal-pedido').modal('hide');
+        $(".pedido").html(tr);
+    }
 })
-//  --------------
 
 function abrir_modal(id,val){
     var quantidade1 = $("#quant").val();
@@ -40,34 +74,42 @@ function abrir_modal(id,val){
 
     switch (id) {
         case "Expresso":
+            $("#add-img").val("IMG/cafeexpresso.jpg"); 
             $("#add-img").attr("src", "IMG/cafeexpresso.jpg"); 
             break;
 
         case "Gourmet":
+            $("#add-img").val("IMG/cafegoumert.jpg"); 
             $("#add-img").attr("src", "IMG/cafegoumert.jpg"); 
             break;
 
         case "Capuccino":
+            $("#add-img").val("IMG/capuccino.jpg"); 
             $("#add-img").attr("src", "IMG/capuccino.jpg"); 
             break;
 
         case "Tradicional":
+            $("#add-img").val("IMG/tradicional.jpeg"); 
             $("#add-img").attr("src", "IMG/tradicional.jpeg"); 
             break;
 
         case "Hambúrguer":
+            $("#add-img").val("IMG/hamburguer.jpg"); 
             $("#add-img").attr("src", "IMG/hamburguer.jpg"); 
             break;
 
         case "Pastel":
+            $("#add-img").val("IMG/pastel.jpg"); 
             $("#add-img").attr("src", "IMG/pastel.jpg"); 
             break;
 
         case "Kibe":
+            $("#add-img").val("IMG/kibe.jpg"); 
             $("#add-img").attr("src", "IMG/kibe.jpg"); 
             break;
 
         default: //Coxinha
+            $("#add-img").val("IMG/coxinha.jpeg"); 
             $("#add-img").attr("src", "IMG/coxinha.jpeg"); 
             break;
     }
